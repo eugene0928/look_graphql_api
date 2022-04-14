b48e0dc89b8d7eb251891d64cda7337e8228d95f
Eugene0928 <umid09tuxtayev@gmail.com>
2022-04-14 20:31:35 +0500

    env port is added

--- src/server.js
+++ src/server.js
@@ -8,6 +8,6 @@ const server = new ApolloServer( {
 } )
 
 
-server.listen().then(({ url }) => {
+server.listen({ port: process.env.Port || 5000 }).then(({ url }) => {
     console.log(`🚀  Server ready at ${url}`)
 })
